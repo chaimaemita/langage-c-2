dffb7551020553e11ac168bd1626331b51f77b8d
chaimae mita <chaimae.mita@gmail.com>
2021-11-14 21:53:19 +0100

    c4

---
+++ c4/CHA 4/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char car;
+    printf("ecrir un caractere\n");
+    scanf("%c",&car);
+
+    switch (car)
+    {
+    case 'a' :
+        printf("voyelle");break;
+    case 'o' :
+        printf("voyelle");break;
+    case 'i' :
+        printf("voyelle");break;
+    case 'e' :
+        printf("voyelle");break;
+    case 'u' :
+        printf("voyelle");break;
+
+    case 'A' :
+        printf("voyelle");break;
+    case 'O' :
+        printf("voyelle");break;
+    case 'I' :
+        printf("voyelle");break;
+    case 'E' :
+        printf("voyelle");break;
+    case 'U' :
+        printf("voyelle");break;
+
+    default:
+        printf("consone");
+    }
+return 0;
+}
